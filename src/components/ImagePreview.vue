e07617af797d0bd5f33ede56f075ff2f423dde24
刘文武 <1344382941@qq.com>
2025-05-06 09:28:19 +0800

    优化

--- src/components/ImagePreview.vue
+++ src/components/ImagePreview.vue
@@ -1,17 +1,6 @@
 <script setup lang="ts">
 import { ElMessage, ElImage } from 'element-plus';
-import { ElIcon } from 'element-plus';
-import {
-  Back,
-  DArrowRight,
-  Download,
-  Refresh,
-  RefreshLeft,
-  RefreshRight,
-  Right,
-  ZoomIn,
-  ZoomOut,
-} from '@element-plus/icons-vue';
+
 
 const props = defineProps<{
   visible: boolean;
@@ -40,24 +29,6 @@ const handleImageLoaded = () => {
   console.log('图片已成功加载');
 };
 
-const download = (index: number) => {
-  const url = props.imageUrl;
-  const suffix = url.slice(url.lastIndexOf('.'));
-  const filename = Date.now() + suffix;
-
-  fetch(url)
-    .then((response) => response.blob())
-    .then((blob) => {
-      const blobUrl = URL.createObjectURL(new Blob([blob]));
-      const link = document.createElement('a');
-      link.href = blobUrl;
-      link.download = filename;
-      document.body.appendChild(link);
-      link.click();
-      URL.revokeObjectURL(blobUrl);
-      link.remove();
-    });
-};
 </script>
 
 <template>
@@ -70,40 +41,20 @@ const download = (index: number) => {
     destroy-on-close
     :show-close="false"
   >
+
     <el-image
       :src="imageUrl"
       :preview-src-list="[imageUrl]"
       fit="contain"
       @error="handleImageError"
       @load="handleImageLoaded"
+      @close="handleClose"
       show-progress
       preview-teleported
+      hide-on-click-modal
     >
-      <template
-        #toolbar="{ actions, prev, next, reset, activeIndex, setActiveItem }"
-      >
-        <el-icon @click="prev"><Back /></el-icon>
-        <el-icon @click="next"><Right /></el-icon>
-        <el-icon @click="setActiveItem(0)">
-          <DArrowRight />
-        </el-icon>
-        <el-icon @click="actions('zoomOut')"><ZoomOut /></el-icon>
-        <el-icon
-          @click="actions('zoomIn', { enableTransition: false, zoomRate: 2 })"
-        >
-          <ZoomIn />
-        </el-icon>
-        <el-icon
-          @click="
-            actions('clockwise', { rotateDeg: 180, enableTransition: false })"
-        >
-          <RefreshRight />
-        </el-icon>
-        <el-icon @click="actions('anticlockwise')"><RefreshLeft /></el-icon>
-        <el-icon @click="reset"><Refresh /></el-icon>
-        <el-icon @click="download(0)"><Download /></el-icon>
-      </template>
     </el-image>
+
   </el-dialog>
 </template>
 
