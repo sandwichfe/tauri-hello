a714d9a13ff2b28d02462d09beb5a18251160611
lww <1344382941@qq.com>
2025-05-10 16:09:31 +0800

    修复当图片预览 超出窗口 会触发父级滚动条的问题

--- src/components/ImagePreview.vue
+++ src/components/ImagePreview.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ElImageViewer  } from 'element-plus';
+import {  ref } from 'vue';
 
 // @ts-ignore-next-line
 const props = defineProps<{
@@ -17,6 +18,27 @@ const handleClose = () => {
   emit('update:visible', false);
 };
 
+const scrollContainer = ref(null);
+
+// 解决当图片预览 超出窗口 会触发父级滚动条的问题
+const handleWheel = (e: WheelEvent) => {
+  const container = scrollContainer.value;
+  if (!container) return; // 确保容器存在
+
+  const { scrollTop, scrollHeight, clientHeight } = container;
+  const isScrollingDown = e.deltaY > 0;
+  const isScrollingUp = e.deltaY < 0;
+
+  // 检查是否到达边界
+  const atTop = scrollTop <= 0 && isScrollingUp;
+  const atBottom = scrollTop + clientHeight >= scrollHeight && isScrollingDown;
+
+  // 阻止默认行为（禁止父级滚动）
+  if (atTop || atBottom) {
+    e.preventDefault();
+  }
+};
+
 
 
 </script>
@@ -25,7 +47,7 @@ const handleClose = () => {
   <!-- <el-dialog :model-value="visible" @update:model-value="emit('update:visible', $event)" width="80%" height="80%"
     @close="handleClose" destroy-on-close :show-close="false"> -->
 
-    <!-- <el-image
+  <!-- <el-image
       :src="imageUrl"
       :preview-src-list="[imageUrl]"
       :preview-teleported="true"
@@ -39,14 +61,15 @@ const handleClose = () => {
     </el-image> -->
   <!-- </el-dialog> -->
 
-
-    <el-image-viewer v-if="visible" 
-    :url-list="imageUrls" 
-    :initial-index="initialIndex"     
-    hide-on-click-modal
-    @close="handleClose"
-    >
+  <div 
+    ref="scrollContainer"
+    @wheel="handleWheel"
+    style="overflow: auto;"
+  >
+    <el-image-viewer v-if="visible" :url-list="imageUrls" :initial-index="initialIndex" hide-on-click-modal
+      @close="handleClose">
     </el-image-viewer>
+  </div>
 
 
 </template>
@@ -54,19 +77,7 @@ const handleClose = () => {
 <style scoped>
 .image-preview {
   width: 100%;
-  max-height: 70vh;
   object-fit: contain;
-}
-
-/* 大图预览的关闭图标 改为大一点的白色 */
-.el-image-viewer__btn.el-image-viewer__close {
-    color: #FFF;
-    opacity: unset;
-    top: 50px;
-    right: 50px;
-    width: 50px;
-    height: 50px;
-    font-size: 50px;
 }
 
 </style>
